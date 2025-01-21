67c16268cd66f255b0d27345d016f624ef84d59c
Abhishek Singh <abhisheksingh4115@gmail.com>
2025-01-21 12:04:40 +0000

    updated tracking driver page

--- src/components/Tracking/Tracking.jsx
+++ src/components/Tracking/Tracking.jsx
@@ -1,5 +1,6 @@
 /** @format */
 
+import Sidebar from '../Sidebar/Sidebar';
 import {
 	Button,
 	TextField,
@@ -31,82 +32,92 @@ const Tracking = () => {
 	if (!isLoaded) return <div>Loading...</div>;
 
 	return (
-		<div className='flex-1 p-6 space-y-6'>
-			{/* Header */}
-			<div className='flex items-center justify-between bg-white shadow rounded-lg p-4'>
-				<div className='flex items-center space-x-4'>
-					<TextField
-						select
-						label='Search by driver'
-						variant='outlined'
-						size='small'
-						style={{ width: '200px' }}
-					>
-						<MenuItem value='all'>All</MenuItem>
-						{/* Add more options as needed */}
-					</TextField>
-					<Button
-						variant='contained'
-						color='primary'
-					>
-						Find
-					</Button>
-				</div>
-				<Button
-					variant='contained'
-					color='secondary'
-				>
-					Re-center
-				</Button>
+		<div className='flex h-screen bg-gray-50'>
+			{/* Sidebar */}
+			<div className='w-80 bg-white shadow-lg border-r'>
+				<Sidebar />
 			</div>
 
-			{/* Map and Table */}
-			<div className='flex space-x-6 h-[calc(100vh-200px)]'>
-				{/* Map Section */}
-				<div className='flex-1 shadow-lg rounded-lg overflow-hidden'>
-					<GoogleMap
-						mapContainerStyle={{ width: '100%', height: '100%' }}
-						center={{ lat: 51.045, lng: -2.272 }}
-						zoom={10}
-					></GoogleMap>
+			{/* Main Content */}
+			<div className='flex-1 p-6 space-y-6'>
+				{/* Header */}
+				<div className='flex items-center justify-between bg-white shadow rounded-lg p-4'>
+					<div className='flex items-center space-x-4'>
+						<TextField
+							select
+							label='Search by driver'
+							variant='outlined'
+							size='small'
+							style={{ width: '200px' }}
+						>
+							<MenuItem value='all'>All</MenuItem>
+							{/* Add more options as needed */}
+						</TextField>
+						<Button
+							variant='contained'
+							color='primary'
+						>
+							Find
+						</Button>
+					</div>
+					<Button
+						variant='contained'
+						color='secondary'
+					>
+						Re-center
+					</Button>
 				</div>
 
-				{/* Table Section */}
-				<div className='w-1/3 bg-white shadow-lg rounded-lg overflow-hidden'>
-					<TableContainer
-						component={Paper}
-						style={{ height: '100%' }}
-					>
-						<Table stickyHeader>
-							<TableHead>
-								<TableRow>
-									<TableCell className='bg-gray-100 font-semibold'>#</TableCell>
-									<TableCell className='bg-gray-100 font-semibold'>
-										Reg
-									</TableCell>
-									<TableCell className='bg-gray-100 font-semibold'>
-										Last
-									</TableCell>
-									<TableCell className='bg-gray-100 font-semibold'>
-										Speed
-									</TableCell>
-								</TableRow>
-							</TableHead>
-							<TableBody>
-								{dataSource.map((row) => (
-									<TableRow
-										key={row.key}
-										hover
-									>
-										<TableCell>{row.key}</TableCell>
-										<TableCell>{row.reg}</TableCell>
-										<TableCell>{row.last}</TableCell>
-										<TableCell>{row.speed}</TableCell>
+				{/* Map and Table */}
+				<div className='flex space-x-6 h-[calc(100vh-200px)]'>
+					{/* Map Section */}
+					<div className='flex-1 shadow-lg rounded-lg overflow-hidden'>
+						<GoogleMap
+							mapContainerStyle={{ width: '100%', height: '100%' }}
+							center={{ lat: 51.045, lng: -2.272 }}
+							zoom={10}
+						></GoogleMap>
+					</div>
+
+					{/* Table Section */}
+					<div className='w-[30%] bg-white shadow-lg rounded-lg overflow-hidden'>
+						<TableContainer
+							component={Paper}
+							style={{ height: '100%' }}
+						>
+							<Table stickyHeader>
+								<TableHead>
+									<TableRow>
+										<TableCell className='bg-gray-100 font-semibold'>
+											#
+										</TableCell>
+										<TableCell className='bg-gray-100 font-semibold'>
+											Reg
+										</TableCell>
+										<TableCell className='bg-gray-100 font-semibold'>
+											Last
+										</TableCell>
+										<TableCell className='bg-gray-100 font-semibold'>
+											Speed
+										</TableCell>
 									</TableRow>
-								))}
-							</TableBody>
-						</Table>
-					</TableContainer>
+								</TableHead>
+								<TableBody>
+									{dataSource.map((row) => (
+										<TableRow
+											key={row.key}
+											hover
+										>
+											<TableCell>{row.key}</TableCell>
+											<TableCell>{row.reg}</TableCell>
+											<TableCell>{row.last}</TableCell>
+											<TableCell>{row.speed}</TableCell>
+										</TableRow>
+									))}
+								</TableBody>
+							</Table>
+						</TableContainer>
+					</div>
 				</div>
 			</div>
 		</div>
